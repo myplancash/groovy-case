cefb5b3ee341e18292dee3ba91a739d3497a5106
sergio esteban torres <myplancash@gmail.com>
2024-05-15 11:57:40 -0500

    Add Reviews component and Phone component
    The Reviews component is added to display reviews on the page. The Phone component is added to display a phone image with an overlaying image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,34 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+// import { Icons } from '@/components/Icons'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Phone from '@/components/Phone'
+import Reviews from '@/components/Reviews'
+import { buttonVariants } from '@/components/ui/button'
 import { ArrowRight, Check, Star } from 'lucide-react'
-import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="bg-slate-50 flex min-h-screen flex-col items-center justify-between p-24">
+    <div className='bg-slate-50 grainy-light'>
       <section>
         <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
           <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
               <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
-                <Image 
-                  src="/groovy-1.png" 
-                  className="w-full" 
-                  alt="logo image" 
-                  width={112}
-                  height={112}
-                />
-                 
+                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
+                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
+                <img src='/groovy-1.png' className='w-full' />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl md:text-7xl">
-                Your Image on a <span className="bg-green-400 px-2 text-white">Custom</span>
-                custom phone Case
+              <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
+                Transform Your Favorite Image into a 
+                <span className='bg-green-600 px-2 text-white'> Bespoke</span> 
+                Phone Case
               </h1>
-              <p className="max-w-prose mt-8">
+              <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
                 Embrace the joy of your cherished moments with your unique 
-                <span>one-of-one</span> phone case. 
-                With Groovy Case, you&apos;re not just safeguarding your phone, you&apos;re preserving your precious memories.
+                <span className='font-semibold'>one-of-one</span> phone case. 
+                With CaseCobra, you're not just safeguarding your phone, you're preserving your precious memories.
               </p>
+
               <ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
                 <div className='space-y-2'>
                   <li className='flex gap-1.5 items-center text-left'>
@@ -47,40 +48,30 @@ export default function Home() {
 
               <div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
                 <div className='flex -space-x-4'>
-                  <Image
+                  <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
                     src='/users/user-1.png'
                     alt='user image'
-                    width={40}
-                    height={40}
                   />
-                  <Image
+                  <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
                     src='/users/user-2.png'
                     alt='user image'
-                    width={40}
-                    height={40}
                   />
-                  <Image
+                  <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
                     src='/users/user-3.png'
                     alt='user image'
-                    width={40}
-                    height={40} 
                   />
-                  <Image
+                  <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
                     src='/users/user-4.jpg'
                     alt='user image'
-                    width={40}
-                    height={40}
                   />
-                  <Image
+                  <img
                     className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
                     src='/users/user-5.jpg'
                     alt='user image'
-                    width={40}
-                    height={40}
                   />
                 </div>
 
@@ -97,12 +88,25 @@ export default function Home() {
                     <span className='font-semibold'>1.250</span> happy customers
                   </p>
                 </div>
-                
               </div>
+            </div>
           </div>
-        </div>
+
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className='relative md:max-w-xl'>
+              <img
+                src='/your-image.png'
+                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+              />
+              <img
+                src='/line.png'
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
-    </div>
-  );
+    </div> 
+  )
 }
